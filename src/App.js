12f0f11f84281e82817b6007fcdfa1d97a6583de
patodekaz <pato@dekaz.cl>
2020-05-23 22:21:42 -0400

    create post and create comment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import PostCreate from './PostCreate'
+import PostList from './PostList'
+export default () => {
+    return <div>
+        <div className="container">
+            <h2>Create Post</h2>
+            <PostCreate/>
+            <hr/>
+            <h1>Posts</h1>
+            <PostList/>
+        </div>
+        
+        </div>
+};
---
+++ src/PostList.js
@@ -0,0 +1,40 @@
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
+import CommentCreate from './CommentCreate'
+
+export default () => {
+    const [ posts, setPosts ] = useState({});
+
+    const fetchPost = async () => {
+        const res = await axios.get('http://localhost:4000/posts');
+        setPosts(res.data);
+    };
+
+    useEffect(() => {
+       fetchPost();
+    }, []);
+
+    const renderedPosts = Object.values(posts).map(post => {
+        return (
+            <div 
+                className="card" 
+                style={{ with: '30%', marginBottom: '20px' }}
+                key={post.id}
+            >    
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                    <CommentCreate/>
+                </div>
+            </div>
+        )
+    })
+
+    // console.log(posts);
+    
+
+    return (
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            { renderedPosts }
+        </div>
+    )
+}
